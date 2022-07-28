a4a534494a537a06f5b1a0fc98ec7d5da97a4b30
lightbuild <renran6303@gmail.com>
2022-07-28 11:22:15 +0800

    加入navigation组件，并设置路由

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import {Routes, Route} from "react-router-dom";
+import {Routes, Route, Outlet} from "react-router-dom";
 import Home from "./routes/home/home.component";
 
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+        <Outlet />
+      </div>
+    </div>
+  )
+}
 
-const App = () =>{
-    return(
-      <Routes>
-          <Route path='/' element={<Home />} />
-      </Routes>
-    )
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />}/>
+      </Route>
+    </Routes>
+  )
 }
 export default App;
