b451559082502349b34adee489d8daca5709f6ea
lightbuild <renran6303@gmail.com>
2022-07-28 14:23:50 +0800

    添加firebase库，添加sign-in组件

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -17,6 +17,9 @@ const Navigation = () => {
           <Link className='nav-link' to='shop'>
             Shop
           </Link>
+          <Link className='sign-in-link' to='signIN'>
+            Sign in
+          </Link>
         </div>
       </div>
       <Outlet />
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,9 @@
+const SignIn = () =>{
+  return(
+    <div>
+      I am  sign in page
+    </div>
+  )
+}
+
+export default SignIn;
