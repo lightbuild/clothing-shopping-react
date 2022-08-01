608911a82c1bb456c994350a9e1ad7702a05224e
lightbuild <renran6303@gmail.com>
2022-08-01 14:42:52 +0800

    sign-up组件添加sign out功能

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,6 @@
 import './sign-up-form.style.scss'
 
-import {useState} from "react";
+import {useState, useContext} from "react";
 
 import {
   createAuthUserWithEmailAndPassword,
@@ -10,6 +10,7 @@ import {
 
 import FormInput from "../form-input/form-input.component";
 import Button from "../button/button.component";
+import {UserContext} from "../../context/user.context";
 
 
 const defaultFormFields = {
@@ -24,6 +25,7 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields)
   const {displayName, email, password, confirmPassword} = formFields
 
+  const {setCurrentUser} = useContext(UserContext)
 
   const resetFormFields =() =>{
       setFormFields(defaultFormFields)
@@ -42,6 +44,8 @@ const SignUpForm = () => {
     }
     try {
       const {user} = await createAuthUserWithEmailAndPassword(email,password)
+      setCurrentUser(user)
+
       await createUserDocumentFromAuth(user,{displayName})
       resetFormFields()
     }catch (error){
