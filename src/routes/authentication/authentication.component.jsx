088c9cac0f9377d7c3871f84db8edb9ffa4c7428
lightbuild <renran6303@gmail.com>
2022-08-01 10:56:03 +0800

    完成authentication页面的样式

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,10 +1,12 @@
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
+import './authentication.style.scss';
+
 const Authentication = () => {
 
   return (
-    <div>
+    <div className='authentication-container'>
       <SignInForm/>
       <SignUpForm/>
     </div>
