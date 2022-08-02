7a4d81d1721cd64f8d0d8be74995858d41e230eb
lightbuild <renran6303@gmail.com>
2022-08-02 11:44:41 +0800

    创建products的context

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import {BrowserRouter} from "react-router-dom";
 import {UserProvider} from "./context/user.context"
+import {ProductsProvider} from "./context/products.context";
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -15,7 +16,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App/>
+        <ProductsProvider>
+          <App/>
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
