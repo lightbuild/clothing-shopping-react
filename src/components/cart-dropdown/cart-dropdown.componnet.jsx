a904dbea2577280a72b1451cb401057b0d72b738
lightbuild <renran6303@gmail.com>
2022-08-02 16:06:13 +0800

    添加cart-item组件

--- src/components/cart-dropdown/cart-dropdown.componnet.jsx
+++ src/components/cart-dropdown/cart-dropdown.componnet.jsx
@@ -1,10 +1,21 @@
 import './cart-dropdown.style.scss'
 import Button from "../button/button.component";
 
+import {useContext} from 'react'
+import {CartContext} from "../../context/cart.context";
+
+import CartItem from "../cart-item/cart-item.component";
+
 const CartDropdown = () =>{
+    const {cartItems} = useContext(CartContext)
+
     return (
       <div className='cart-dropdown-container'>
-        <div className='cart-items'>ss</div>
+        <div className='cart-items'>
+          {cartItems.map((item) =>(
+            <CartItem key={item.id} cartItem={item} />
+          ))}
+        </div>
         <Button>Go to Checkout</Button>
       </div>
     )
--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,13 +1,26 @@
-import {createContext,useState} from 'react'
+import {createContext,useState,useEffect} from 'react'
+
+import PRODUCTS_DATA from '../shop-data.json'
+
+
+
 
 export const CartContext = createContext({
   isCartOpen:false,
-  setIsCartOpen:()=>{}
+  setIsCartOpen:()=>{},
+  cartItems:[],
+  addItemToCart:()=>{},
+  removeItemFromCart:()=>{},
+  clearItemFromCart:()=>{},
 })
 
 export const CartProvider = ({children}) =>{
     const [isCartOpen,setIsCartOpen] = useState(false)
-    const value = {isCartOpen,setIsCartOpen}
+    const [cartItems,setCartItems] = useState(PRODUCTS_DATA)
+
+
+
+    const value = {isCartOpen,setIsCartOpen,cartItems,setCartItems}
     return(
       <CartContext.Provider value={value}>{children}</CartContext.Provider>
     )
