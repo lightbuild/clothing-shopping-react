be34ce3e91614321e4924e691bffdb46c87c1afe
lightbuild <renran6303@gmail.com>
2022-08-03 11:56:16 +0800

    完成cart-icon计数功能

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -33,6 +33,7 @@ const removeCartItem = (cartItems, productToRemove) => {
 const clearCartItem = (cartItems, cartItemToClear) =>
   cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
 
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
@@ -40,18 +41,56 @@ export const CartContext = createContext({
   addItemToCart: () => {},
   removeItemFromCart: () => {},
   clearItemFromCart: () => {},
+  cartCount:0,
+  cartTotal:0
 })
 
 export const CartProvider = ({children}) => {
   const [isCartOpen, setIsCartOpen] = useState(false)
   const [cartItems, setCartItems] = useState([])
 
+  const [cartCount, setCartCount] = useState(0)
+  const [cartTotal, setCartTotal] = useState(0)
+
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartTtem) => total + cartTtem.quantity,
+      0
+    )
+    setCartCount(newCartCount)
+  }, [cartItems])
+
+  useEffect(() => {
+    const newCartTotal = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity * cartItem.price,
+      0
+    )
+    setCartTotal(newCartTotal)
+  }, [cartItems])
+
   const addItemToCart = (productToAdd) => {
     setCartItems(addCartItem(cartItems, productToAdd))
   }
 
+  const removeItemFromCart = (productToRemove) => {
+    setCartItems(removeCartItem(cartItems, productToRemove))
+  }
 
-  const value = {isCartOpen, setIsCartOpen, cartItems, addItemToCart}
+  const clearItemFromCart = (productToClear) => {
+    setCartItems(clearCartItem(cartItems, productToClear))
+  }
+
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    cartItems,
+    addItemToCart,
+    removeItemFromCart,
+    clearItemFromCart,
+    cartCount,
+    cartTotal
+  }
   return (
     <CartContext.Provider value={value}>{children}</CartContext.Provider>
   )
