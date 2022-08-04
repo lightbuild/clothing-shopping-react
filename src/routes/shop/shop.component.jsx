881cebac0593678ed1e67182035c303d5898c2d2
lightbuild <renran6303@gmail.com>
2022-08-04 12:13:58 +0800

    渲染后台数据到shop组件

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,18 +1,29 @@
-import {useContext} from 'react'
-import {ProductsContext} from "../../context/products.context";
+import {useContext, Fragment} from 'react'
+import {CategoriesContext} from "../../context/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
 
 import './shop.style.scss'
 
-const Shop = () =>{
-  const {products} = useContext(ProductsContext)
-    return(
-      <div className='products-container'>
-        {products.map((product) =>(
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
-    )
+const Shop = () => {
+  const {categoriesMap} = useContext(CategoriesContext)
+
+  return (
+    <Fragment>
+      {
+        Object.keys(categoriesMap).map((title) => (
+          <Fragment key={title}>
+            <h2>{title}</h2>
+            <div className='products-container'>
+              {categoriesMap[title].map((product) => (
+                <ProductCard key={product.id} product={product}/>
+              ))}
+            </div>
+          </Fragment>
+        ))
+      }
+    </Fragment>
+
+  )
 }
 
 export default Shop
