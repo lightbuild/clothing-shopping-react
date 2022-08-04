5dbeebee79b9a469bf97e0079f7c84cc545b002a
lightbuild <renran6303@gmail.com>
2022-08-04 16:52:47 +0800

    引入styled-components库

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,11 +1,13 @@
 import './category-preview.style.scss'
 import ProductCard from "../product-card/product-card.component";
 
+import {Link} from 'react-router-dom'
+
 const CategoryPreview = ({title,products}) =>{
     return (
       <div className='category-preview-container'>
         <h2 className='title-container'>
-          <span className='title'>{title}</span>
+          <Link className='title' to={title}>{title.toUpperCase()}</Link>
         </h2>
         <div className='preview'>
           {
