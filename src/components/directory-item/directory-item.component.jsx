89751fe09ff497e40075748528018ccbef928b12
lightbuild <renran6303@gmail.com>
2022-08-06 15:54:21 +0800

    为directory添加shop跳转路由

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,9 +1,18 @@
-import {BackgoundImage,DirectotyBodyContainer,DirectoryContainer} from './directory-item.style'
+import {useNavigate} from 'react-router-dom'
+
+import {
+  BackgoundImage,
+  DirectotyBodyContainer,
+  DirectoryContainer} from './directory-item.style'
 
 const DirectoryItem = ({category}) => {
-  const {imageUrl,title} = category;
+  const {imageUrl,title,route} = category;
+  const navigate = useNavigate()
+
+  const onNavigeteHandler = () => navigate(route)
+
   return (
-    <DirectoryContainer>
+    <DirectoryContainer onClick={onNavigeteHandler}>
       <BackgoundImage imageUrl={imageUrl}/>
       <DirectotyBodyContainer>
         <h2>{title}</h2>
