dad78fbdd622fb71cbdfac73f6bafc987f1f83cd
lightbuild <renran6303@gmail.com>
2022-08-08 17:53:23 +0800

    使用ruducer修改use.context

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,4 @@
-import {createContext, useState, useEffect} from 'react'
+import {createContext,useEffect,useReducer} from 'react'
 
 import {createUserDocumentFromAuth, onAuthStateChangedListener} from '../utils/firebase/firebase.utils'
 
@@ -7,9 +7,40 @@ export const UserContext = createContext({
   setCurrentUser: () => null
 })
 
+export const USER_ACTION_TYPE = {
+  SET_CURRENT_TYPE:'SET_CURRENT_TYPE'
+}
+
+const userReducer = (state,action) =>{
+  console.log('dispatched')
+  console.log('action',action)
+  console.log('state',state)
+  const {type,payload} = action
+
+  switch (type){
+    case USER_ACTION_TYPE.SET_CURRENT_TYPE:
+      return {
+        ...state,
+        currentUser: payload
+      }
+    default:
+      throw new Error('数据与userReducer不匹配')
+  }
+}
+
+const INITIAL_USER = {
+  currentUser:null
+}
+
 export const UserProvider = ({children}) => {
-  const [currentUser, setCurrentUser] = useState(null)
+  const [{currentUser},dispatch] = useReducer(userReducer,INITIAL_USER);
+  const setCurrentUser= (user) =>{
+      dispatch({type:USER_ACTION_TYPE.SET_CURRENT_TYPE,payload:user})
+  }
+
+
   const value = {currentUser, setCurrentUser}
+
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener(async (user) => {
       if(user){
