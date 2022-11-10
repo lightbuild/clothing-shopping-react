f48530b15a544664267b8462711904fa448e936b
Lightbuild <893458528@qq.com>
2022-11-10 15:20:03 +0800

    修复跳转结算页面后购物框还存在的问题

--- src/components/cart-dropdown/cart-dropdown.componnet.tsx
+++ src/components/cart-dropdown/cart-dropdown.componnet.tsx
@@ -1,21 +1,24 @@
-import {CartDropdownContainer,CartItems,EmptyMessage} from './cart-dropdown.style'
-import Button from "../button/button.component";
+import {CartDropdownContainer, CartItems, EmptyMessage} from './cart-dropdown.style';
+import Button from '../button/button.component';
 
 import {useNavigate} from 'react-router-dom';
 
-import {useSelector} from 'react-redux'
-import {selectCartItems} from "../../store/cart/cart.selector";
+import {useDispatch, useSelector} from 'react-redux';
+import {selectCartItems} from '../../store/cart/cart.selector';
 
-import CartItem from "../cart-item/cart-item.component";
+import CartItem from '../cart-item/cart-item.component';
+import {setIsCartOpen} from '../../store/cart/cart.action';
 
 
 const CartDropdown = () => {
-  const cartItems = useSelector(selectCartItems)
+  const dispatch = useDispatch();
+  const cartItems = useSelector(selectCartItems);
 
-  const navigation = useNavigate()
+  const navigation = useNavigate();
   const goToCheckoutHandler = () => {
-    navigation('/checkout')
-  }
+    navigation('/checkout');
+    dispatch(setIsCartOpen(false));
+  };
 
   return (
     <CartDropdownContainer>
@@ -23,16 +26,15 @@ const CartDropdown = () => {
         {cartItems.length ?
           (
             cartItems.map((item) => (
-            <CartItem key={item.id} cartItem={item}/>
-          ))) : (
+              <CartItem key={item.id} cartItem={item}/>
+            ))) : (
             <EmptyMessage>Your cart is empty</EmptyMessage>
           )
-
         }
       </CartItems>
       <Button onClick={goToCheckoutHandler}>Go to Checkout</Button>
     </CartDropdownContainer>
-  )
-}
+  );
+};
 
-export default CartDropdown
+export default CartDropdown;
